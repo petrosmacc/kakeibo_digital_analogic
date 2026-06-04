61d505c97a479207f03983b812be049b8c9028e8
Pedro Henrique Machado Valgas <petrosmacchado@gmail.com>
2026-06-03 23:58:00 -0300

    fix: ajustes residuais no graficos.js

--- js/ui/graficos.js
+++ js/ui/graficos.js
@@ -8,13 +8,3 @@ export function renderGraficos(state) {
         </section>
     `;
 }
-export function renderGraficos(state) {
-    return `
-        <section class="card">
-            <h2>Análise de Gastos</h2>
-            <div id="chart-container">
-                <p class="empty-msg">Gráficos em desenvolvimento...</p>
-            </div>
-        </section>
-    `;
-}
